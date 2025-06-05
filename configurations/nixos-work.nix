cc87a4739814cad1ed8a3d16195fa583dbd5745c
Backendt <matisse.beckandt@proton.me>
2025-06-05 11:08:47 +0200

    Added Ferdium to work configuration

--- configurations/nixos-work.nix
+++ configurations/nixos-work.nix
@@ -1,6 +1,12 @@
+{ pkgs, ... }:
+
 {
     imports = [ ./hardware/work-hardware-configuration.nix ];
 
     networking.hostName = "nixos-work";
     boot.kernelParams = [ "iommu=soft" ];
+
+    environment.systemPackages = [
+        pkgs.ferdium
+    ];
 }

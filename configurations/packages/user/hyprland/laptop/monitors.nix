1d522dd09c8df55367e9f70bbe19ab7fb8654a91
Backendt <matisse.beckandt@proton.me>
2025-06-10 12:51:57 +0200

    Changed laptop monitor layout depending on port used

--- configurations/packages/user/hyprland/laptop/monitors.nix
+++ configurations/packages/user/hyprland/laptop/monitors.nix
@@ -4,6 +4,8 @@
     wayland.windowManager.hyprland.settings = {
         monitor = [
             "eDP-1, 1920x1200, 0x0, 1.5"
+            "DP-1, preferred, auto-right, 1"
+            "DP-2, preferred, auto-left, 1"
             "Unknown-1, disable"
             " , preferred, auto-left, 1"
         ];

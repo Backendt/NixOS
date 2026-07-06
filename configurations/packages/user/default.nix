4fb67a1f2e2fee143422ba63281bad375eef2b64
Backendt <matisse.beckandt@proton.me>
2026-07-06 14:28:08 +0200

    [ADD] Added Obsidian, wireless tools and image viewer

--- configurations/packages/user/default.nix
+++ configurations/packages/user/default.nix
@@ -36,6 +36,9 @@
                     dust
                     tree
                     jq
+                    wirelesstools
+                    imv
+                    obsidian
                 ];
             }
         ];
